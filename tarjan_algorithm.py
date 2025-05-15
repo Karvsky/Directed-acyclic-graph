b77022963e804375867973feabd461d3f66e7610
Karvsky <karvsky@gmail.com>
2025-05-15 19:58:16 +0200

    12

--- tarjan_algorithm.py
+++ tarjan_algorithm.py
@@ -1,25 +1,25 @@
-def Tarjan(graph, n):
-    oznaczony = ['nieoznaczony']*n
+def Tarjan(adj_matrix, n):
+    status = [0] * n
     L = []
-    print(f"Rozpoczynamy algorytm Tarjana dla grafu o {n} wierzchołkach.")
-    for i in range(n):
-        if oznaczony[i] == 'nieoznaczony':
-            print(f"Odwiedzamy wierzchołek {i}")
-            visit(i, oznaczony, graph, L) 
-    if L:
-        return L[::-1]
-    else:
-        print("Graf jest acykliczny, ale brak porządku topologicznego.")
-def visit(n, oznaczony, graph, L):
-    if oznaczony[n] == 'staly':
-        return
-    elif oznaczony[n] == 'tymczasowy':
-        print("graf ma cykl")
-        return
-    oznaczony[n] = 'tymczasowy'
-    for i in range(n):
-        if graph[n][i] == 1:
-            visit(i, oznaczony, graph, L)
-    oznaczony[n] = 'staly'
-    L.append(n)
+    
+    def visit(node):
+        if status[node] == 2:
+            return
+        if status[node] == 1:
+            raise ValueError("Graf zawiera cykl - sortowanie topologiczne niemożliwe")
+        
+        status[node] = 1
+        
+        for next_node in range(n):
+            if adj_matrix[node][next_node] == 1:
+                visit(next_node)
+        
+        status[node] = 2
+        L.append(node)
+    
+    for node in range(n):
+        if status[node] == 0:
+            visit(node)
+    
+    print (L[::-1])
 
